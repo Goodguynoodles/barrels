026503011c7d466dbc607a10f59a3a7f32527d06
koos van losenoord <316595@student.mboutrecht.nl>
2020-03-30 22:43:41 +0200

    no message

---
+++ Assets/script/gamemanager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamemanager : MonoBehaviour
+{
+    public void hardRestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+        Debug.Log("test");
+    }
+}
